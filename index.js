fa746c0e05f62f68c92c664fd41d89e3450f784c
Harmeet Matharoo <hmjatt@gmail.com>
2022-07-11 11:50:08 -0600

    Create a `twitterController.js`
    Create a `twitterController.js` file which collects data from **Twitter API**.
    
    moved modules to separate folder

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const client = require("./twitterClient");
+const client = require("./public/modules/twitterClient");
 
 // async function testTweet() {
 //     await client.v2.tweet("Hi, I am building a twitter bot!")
