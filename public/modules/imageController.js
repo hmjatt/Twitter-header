4b07822b46b9c17d846fea312855d68342a33c99
Harmeet Matharoo <hmjatt@gmail.com>
2022-07-11 13:25:49 -0600

    formatted code with prettier

---
+++ public/modules/imageController.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+const sharp = require("sharp");
+
+async function saveImage(name, url) {
+    const { data } = await axios.get(url, {
+        responseType: "arraybuffer",
+    });
+
+    await sharp(data).resize(100, 100).toFile(`./images/${name}.png`);
+}
+
+module.exports = { saveImage };
